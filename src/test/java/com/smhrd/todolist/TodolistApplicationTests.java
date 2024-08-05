c410a98d50b6dab1b96c201d885e0135a9130050
김해공 <rlagorhd@gmail.com>
2024-08-05 16:29:30 +0900

    sec

---
+++ src/test/java/com/smhrd/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
