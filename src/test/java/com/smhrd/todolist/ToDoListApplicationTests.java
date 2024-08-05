c281345f2be7e550084b2cd83334e683155ad8d9
Jo <mz8152@naver.com>
2024-08-05 14:18:08 +0900

    start

---
+++ src/test/java/com/smhrd/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
