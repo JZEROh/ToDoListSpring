8233d2da545c0b703c2022a3cd57a43d55db4004
김해공 <rlagorhd@gmail.com>
2024-08-06 14:56:16 +0900

    memberControllerJoinChange

--- src/main/java/com/smhrd/todolist/Controller/MemberController.java
+++ src/main/java/com/smhrd/todolist/Controller/MemberController.java
@@ -2,9 +2,11 @@ package com.smhrd.todolist.Controller;
 
 import com.smhrd.todolist.Model.Member;
 import com.smhrd.todolist.Repository.MemberRepository;
+import com.smhrd.todolist.Service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,14 +14,14 @@ public class MemberController {
 
     @Autowired
     private MemberRepository Repository;
+    @Autowired
+    private MemberService memberService;
 
     // 회원가입
     @PostMapping("/join")
-    public void join(HttpServletRequest request) {
-        String param = request.getParameter("joinMember");
-        System.out.println(param);
-
-
+    public void join(@RequestBody Member member) {
+        memberService.join(member);
+        System.out.println(member);
     }
     // 로그인
 
